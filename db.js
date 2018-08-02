dbfba1a57182e40f320ccd3e3653548bda6793ba
Sasuke1964 <neilperry1964@gmail.com>
2018-08-02 15:09:01 -0500

    added password input for db.js

--- db.js
+++ db.js
@@ -1,8 +1,12 @@
 var mysql = require('mysql');
+
+let args = process.argv.slice(2);
+let pass = args[0];
+
 var connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
-	password: 'Enter_Password',
+	password: pass,
 	database: 'music_recommender'
 });
 

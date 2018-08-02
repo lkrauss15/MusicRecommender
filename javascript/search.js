9424f3826238d5122c3e3fbbff1664a374a8f942
Luke Krauss <lukekrauss15@gmail.com>
2018-08-02 12:49:02 -0500

    Added jQuery

---
+++ javascript/search.js
@@ -0,0 +1,6 @@
+$("#searchButton").click(function(){
+    alert("hello");
+    //$.post("localhost:4000/results.html", function(data, status){
+    //    alert("Data: " + data + "\nStatus: " + status);
+    //});
+})

458d82cb98d1f331531ecd158f0cbd94623a902e
Sasuke1964 <neilperry1964@gmail.com>
2018-08-02 16:01:07 -0500

    adding db to server.js

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 var express = require('express');
+var mysql = require('mysql');
+
+let args = process.argv.slice(2);
+let pass = args[0];
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: pass,
+  database: 'music_recommender'
+});
+
+connection.connect();
+
 var app = express();
 
 var bodyParser = require('body-parser');
@@ -11,6 +25,11 @@ app.use(function (req, res, next) {
 });
 
 app.get('/search', function (req, res) {
+
+  connection.query("SELECT s.name, s.listeners, a.name, a.url FROM music_recommender.song s, music_recommender.artist a where s.name = 'Va Va Voom' and s.createdBy = a.artistID;", function (error, results, fields) {
+    if (error) throw error;
+    console.log(results);
+  });
   
   res.send({resonse: "Response in this Object"});
 });
